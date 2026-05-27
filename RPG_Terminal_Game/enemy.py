d4c1079e561084b573337f26675136dd884acaf4
stuti-sudo-123 <stutigupta2007@gmail.com>
2026-05-27 17:13:57 +0530

    enemy class updated

--- RPG_Terminal_Game/enemy.py
+++ RPG_Terminal_Game/enemy.py
@@ -1,54 +1,54 @@
 import random
-ENEMY_TYPES={
-        "goblin": {
-                "name":    "Goblin",       
-                "health":  30,              
-                "attack":  8,              
-                "defense": 2,              
-                "xp":      20,              
-                "gold":    (2, 6),         
-                "emoji":   "👺",  
-                "loot":    [               
-                    {"name": "Health Potion", "type": "heal", "value": 25, "chance": 0.3}
-                ]
-            },
-        "skeleton":{
-                "name":    "Skeleton",        
-                "health":  45,              
-                "attack":  12,               
-                "defense": 4,               
-                "xp":      35,              
-                "gold":    (4, 10),          
-                "emoji":   "💀",            
-                "loot":    [                
-                    {"name": "Health Potion", "type": "heal", "value": 25, "chance": 0.25}
-                ]
-            },
-        "orc":{
-                "name":    "Orc",        
-                "health":  70,              
-                "attack":  18,              
-                "defense": 8,               
-                "xp":      55,              
-                "gold":    (8, 18),
-                "emoji":   "👹",            
-                "loot":    [                
-                    {"name": "Health Potion", "type": "heal", "value": 25, "chance": 0.4}
-                ]
-            },
-        "dragon":{
-                "name":    "Dragon",        
-                "health":  150,              
-                "attack":  30,               
-                "defense": 15,               
-                "xp":      150,              
-                "gold":    (30, 60),         
-                "emoji":   "🐉",           
-                "loot":    [                
-                    {"name": "Large Potion", "type": "heal", "value": 50, "chance": 0.8}
-                ]
-            }
-        }
+ENEMY_TYPES = {
+    "spore_crawler": {
+        "name":    "Spore Crawler",
+        "health":  30,
+        "attack":  8,
+        "defense": 2,
+        "xp":      20,
+        "gold":    (2, 6),
+        "emoji":   "🍄",
+        "loot":    [
+            {"name": "Nano Injector", "type": "heal", "value": 25, "chance": 0.3}
+        ]
+    },
+    "void_shade": {
+        "name":    "Void Shade",
+        "health":  45,
+        "attack":  12,
+        "defense": 4,
+        "xp":      35,
+        "gold":    (4, 10),
+        "emoji":   "👻",
+        "loot":    [
+            {"name": "Nano Injector", "type": "heal", "value": 25, "chance": 0.25}
+        ]
+    },
+    "titan_brute": {
+        "name":    "Titan Brute",
+        "health":  70,
+        "attack":  18,
+        "defense": 8,
+        "xp":      55,
+        "gold":    (8, 18),
+        "emoji":   "🗿",
+        "loot":    [
+            {"name": "Plasma Cell", "type": "heal", "value": 50, "chance": 0.4}
+        ]
+    },
+    "ancient_one": {
+        "name":    "The Ancient One",
+        "health":  150,
+        "attack":  30,
+        "defense": 15,
+        "xp":      150,
+        "gold":    (30, 60),
+        "emoji":   "🌌",
+        "loot":    [
+            {"name": "Void Essence", "type": "heal", "value": 100, "chance": 0.8}
+        ]
+    }
+}
 class Enemy:
     def __init__(self,enemy_type):
         data=ENEMY_TYPES.get(enemy_type.lower())
